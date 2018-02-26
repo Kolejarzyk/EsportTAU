2c9d34eaa75b686f37697e048cac8562251f7c73
Paweł Dering <s13905@lab110l-07>
2018-02-26 15:42:13 +0100

    match fixes

--- lab01/src/main/java/pl/kolejarz/app/Match.java
+++ lab01/src/main/java/pl/kolejarz/app/Match.java
@@ -11,7 +11,7 @@ public class Match
     private String gameName;
 
 
-    public Match(String nameTeam1,String nameTeam2, int scoreTeam1, int scoreTeam2, int gameName) {
+    public Match(String nameTeam1,String nameTeam2, int scoreTeam1, int scoreTeam2, String gameName) {
         this.nameTeam1 = nameTeam1;
         this.nameTeam2 = nameTeam2;
         this.scoreTeam1 = scoreTeam1;
